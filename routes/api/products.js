f31e0e0c1b66394214517d51e197fcaf0ba60dec
github <pragya.sharma2_cs19@gla.ac.in>
2021-09-19 17:35:08 +0530

    define post and get functions for Product

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,4 +1,15 @@
 const route = require('express').Router()
+const Product = require('../../db/db').Product
+
+route.get('/',(req,res)=>{
+
+})
+
+route.exports('/',(req,res)=>{
+    
+})
+
+
 
 exports = module.exports={
     route
