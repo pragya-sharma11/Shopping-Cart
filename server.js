42af46b6dd41872e9dbbee81f6c4bf120a9ebb55
github <pragya.sharma2_cs19@gla.ac.in>
2021-09-19 17:14:51 +0530

    url endoded for server url

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+
+app.use(express.json())
+app. use(express.urlencoded({extended:true}))
+
 app.use('/', express.static(path.join(__dirname,'public')))
 
 app.use('/api', require('./routes/api').route)
@@ -10,6 +14,8 @@ app.use('/api', require('./routes/api').route)
  * tries to find s.json,if it is absent 
  * then it tries to find index.js inside x i.e. -> 'x/index.js'
  */
+
+
 app.listen(2678,()=>{
     console.log("server start on http://localhost:2678")
 })
