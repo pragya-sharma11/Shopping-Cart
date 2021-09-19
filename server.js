7e04eb76af7f9a453996b598e55d805a933440a2
github <pragya.sharma2_cs19@gla.ac.in>
2021-09-19 17:02:33 +0530

    add comment for require

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const app = express()
 app.use('/', express.static(path.join(__dirname,'public')))
 
 app.use('/api', require('./routes/api').route)
-
+/**
+ * require('x')
+ * it tries to find x.js if it's not present then it 
+ * tries to find s.json,if it is absent 
+ * then it tries to find index.js inside x i.e. -> 'x/index.js'
+ */
 app.listen(2678,()=>{
     console.log("server start on http://localhost:2678")
 })
