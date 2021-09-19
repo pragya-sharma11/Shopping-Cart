359e913591f76b340687decac8e71b9329c8e996
github <pragya.sharma2_cs19@gla.ac.in>
2021-09-19 17:37:55 +0530

    define get function for products

--- routes/api/products.js
+++ routes/api/products.js
@@ -2,11 +2,18 @@ const route = require('express').Router()
 const Product = require('../../db/db').Product
 
 route.get('/',(req,res)=>{
-
+    //get all products
+    Product.findAll().then((products)=>{
+        res.status(200).send(products)
+    }).catch(()=>{
+        res.status(500).send({
+            error:"Could not get the products."
+        })
+    })
 })
 
 route.exports('/',(req,res)=>{
-    
+    //post all products to db.
 })
 
 
