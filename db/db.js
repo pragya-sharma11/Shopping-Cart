aca0fa082c58bc9fc5cc64f60ad740b20a44ff7e
github <pragya.sharma2_cs19@gla.ac.in>
2021-09-19 15:09:10 +0530

    commit

--- db/db.js
+++ db/db.js
@@ -8,3 +8,32 @@ const db = new Sequelize('shopdb','shopper', 'shoppass',{
     }
 })
 
+const u=User= db.define('users',{
+    id:{
+        type:Sequelize.INTEGER,
+        autoIncrement:true,
+        primaryKey:true
+    },
+    name:{
+        type:Sequelize.STRING,
+        allowNull:false
+    }
+})
+
+const Product = db.defin('products',{
+    id:{
+        type:Sequelize.INTEGER,
+        autoIncrement:true,
+        primaryKey:true
+    },
+    name:{
+        type:Sequelize.STRING,
+        allowNull:false
+    },
+    manufacturer:Sequelize.STRING,
+    price:{
+        type:Sequelize.FLOAT,
+        allowNull:false,
+        default:0.0
+    }
+})
